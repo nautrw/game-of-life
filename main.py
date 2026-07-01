a4cea471c9cb39284b25cb66a15c0df65e6d4d28
nautrw <nautrw@proton.me>
2026-07-01 04:47:11 -0400

    use a class for the board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-BLOCK_SIZE = 20
+CELL_SIZE = 20
 GRID_COLOR = (50, 50, 50)
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -10,18 +10,36 @@ SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
-def draw_grid():
-    for i in range(0, WINDOW_WIDTH, BLOCK_SIZE):
-        pygame.draw.line(SCREEN, GRID_COLOR, (i, 0), (i, WINDOW_HEIGHT))
-        pygame.draw.line(SCREEN, GRID_COLOR, (0, i), (WINDOW_WIDTH, i))
+class Board:
+    def __init__(self, width, height, cell_size):
+        self.rows = height // cell_size
+        self.columns = width // cell_size
+        self.cell_size = cell_size
+        self.cells = [
+            [False for _ in range(self.columns)]
+            for _ in range(self.rows)
+        ]
+        self.cells[0][1] = True
+    
+    def draw(self, screen, border_width=1, dead_color="black", alive_color="white", border_color="gray20"):
+        screen.fill(border_color)
+        
+        for row in range(self.rows):
+            for column in range(self.columns):
+                cell = self.cells[row][column]
+                cell_color = dead_color if not cell else alive_color
+                x = column * self.cell_size
+                y = row * self.cell_size
+                
+                pygame.draw.rect(screen, cell_color, (x, y, self.cell_size - border_width, self.cell_size - border_width))
 
+board = Board(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    SCREEN.fill("black")
-    draw_grid()
+    board.draw(SCREEN, 1)
     
     pygame.display.flip()
     clock.tick(60)
