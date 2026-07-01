c8460d32f9c7860d398aa8ce3f6fac8b09f8ace0
nautrw <nautrw@proton.me>
2026-07-01 04:04:24 -0400

    add function to draw the grid

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 
+BLOCK_SIZE = 20
+GRID_COLOR = (50, 50, 50)
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 running = True
+
+def draw_grid():
+    for i in range(0, WINDOW_WIDTH, BLOCK_SIZE):
+        pygame.draw.line(SCREEN, GRID_COLOR, (i, 0), (i, WINDOW_HEIGHT))
+        pygame.draw.line(SCREEN, GRID_COLOR, (0, i), (WINDOW_WIDTH, i))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("black")
+    SCREEN.fill("black")
+    draw_grid()
     
     pygame.display.flip()
     clock.tick(60)
