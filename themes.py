7c5375c4af8002ba8a2294fe0e4e27ae968d9e53
nautrw <nautrw@proton.me>
2026-07-02 07:33:22 -0400

    push the themes file

---
+++ themes.py
@@ -0,0 +1,47 @@
+THEMES = [
+    {
+        "dead_color": "black",
+        "alive_color": "white",
+        "border_color": "gray20",
+    },
+    {
+        "dead_color": "black",
+        "alive_color": "lime",
+        "border_color": "darkgreen",
+    },
+    {
+        "dead_color": "midnightblue",
+        "alive_color": "deepskyblue",
+        "border_color": "steelblue4",
+    },
+    {
+        "dead_color": "midnightblue",
+        "alive_color": "orange",
+        "border_color": "orangered4",
+    },
+    {
+        "dead_color": "black",
+        "alive_color": "red",
+        "border_color": "darkred",
+    },
+    {
+        "dead_color": "gray10",
+        "alive_color": "cyan",
+        "border_color": "steelblue",
+    },
+    {
+        "dead_color": "gray8",
+        "alive_color": "orchid",
+        "border_color": "mediumpurple4",
+    },
+    {
+        "dead_color": "gray10",
+        "alive_color": "hotpink",
+        "border_color": "deeppink4",
+    },
+    {
+        "dead_color": "#002b36",
+        "alive_color": "#b58900",
+        "border_color": "#073642",
+    },
+]
